79da4924577142996213b0e6177ea963b7ca6fd2
sam-gordon-f <sam.gordon.f@gmail.com>
2022-09-28 15:20:23 +1000

    forcing helm release

--- modules/kubernetes-addons/newrelic/main.tf
+++ modules/kubernetes-addons/newrelic/main.tf
@@ -1,6 +1,6 @@
 module "helm_addon" {
   source            = "../helm-addon"
-  manage_via_gitops = var.manage_via_gitops
+  # manage_via_gitops = var.manage_via_gitops
   helm_config       = local.helm_config
   irsa_config       = null
   addon_context     = var.addon_context
