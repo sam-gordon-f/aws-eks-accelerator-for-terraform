35f33ac88aa659efbbe3833d4db7504d55f178fa
sam-gordon-f <sam.gordon.f@gmail.com>
2022-09-30 11:23:48 +1000

    setting exact url for chart

--- modules/kubernetes-addons/harness-cd-ce/locals.tf
+++ modules/kubernetes-addons/harness-cd-ce/locals.tf
@@ -3,9 +3,7 @@ locals {
 
   default_helm_config = {
     name        = local.name
-    chart       = "harness"
-    repository  = "https://github.com/harness/harness-cd-community/tree/main/helm"
-    version     = ""
+    chart       = "https://github.com/harness/harness-cd-community/tree/main/helm/harness"
     namespace   = local.name
     description = "Harness CD - community edition"
     values      = local.default_helm_values
